4a4e791501a4c1e815bb1e10015c05b92b37cb48
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2020-04-22 10:06:17 -0500

    Fix type relation and refactor

--- src/main.js
+++ src/main.js
@@ -30,27 +30,23 @@ fetch("http://localhost:1111/src/grants.json")
     values(json);
   });
 
+var getColor = function(type) {
+  const types = [{objType: "ROLE", color: 'lightgreen'}, 
+                 {objType: "USER", color: '#D2E5FF'}];
+  const x = types.find(element => element.objType === type).color;
+  if(type === 'USER'){
+    console.log(type);
+  }
+  return x;
+}
+
 var x = [];
 var y = new vis.DataSet([]);
 var values = function(json){
-  json.forEach(element => {
-    var xbgcolor = '', ybgcolor  = '';
-    
-      if((element.GRANTED_TO_TYPE === 'USER' || element.GRANTED_TO_TYPE === 'ROLE' ) && (element.SNOWFLAKE_OBJECT_TYPE === 'ROLE'  || element.SNOWFLAKE_OBJECT_TYPE === 'USER')){   
-
-
-        if(element.GRANTED_TO_TYPE === 'ROLE'){
-          xbgcolor = 'lightgreen'
-        } else if(element.GRANTED_TO_TYPE === 'USER'){
-          xbgcolor = '#D2E5FF'
-        }
-        if(element.SNOWFLAKE_OBJECT_TYPE === 'ROLE'){
-          ybgcolor = 'lightgreen'
-        } else if(element.SNOWFLAKE_OBJECT_TYPE === 'USER'){
-          ybgcolor = '#D2E5FF'
-        }
+  json.forEach(element => {    
+    if((element.GRANTED_TO_TYPE === 'USER' || element.GRANTED_TO_TYPE === 'ROLE' ) && (element.SNOWFLAKE_OBJECT_TYPE === 'ROLE'  || element.SNOWFLAKE_OBJECT_TYPE === 'USER')){   
         try {
-        y.add({id: element.GRANTED_TO_NAME, label: element.GRANTED_TO_NAME, shape: "box", color: { border: '#2B7CE9', background: xbgcolor,
+          nodes.add({id: element.GRANTED_TO_NAME, label: element.GRANTED_TO_NAME, shape: "box", color: { border: '#2B7CE9', background: getColor(element.GRANTED_TO_TYPE),
           highlight: {
             border: '#2B7CE9',
             background: '#D2E5FF'
@@ -63,24 +59,21 @@ var values = function(json){
         //errors if there are duplicate items
       }
       try {
-        y.add({id: element.OBJECT_NAME, label: element.OBJECT_NAME, shape: "box", color: { border: '#2B7CE9', background: ybgcolor,
-        highlight: {
-          border: '#2B7CE9',
-          background: '#D2E5FF'
-        }},
-        widthConstraint: { maximum: 150 }
-      });
-    }
-    catch(error) {
-      console.log(error);
-        //errors if there are duplicate items
-    }
+        nodes.add({id: element.OBJECT_NAME, label: element.OBJECT_NAME, shape: "box", color: { border: '#2B7CE9', background: getColor(element.SNOWFLAKE_OBJECT_TYPE),
+          highlight: {
+            border: '#2B7CE9',
+            background: '#D2E5FF'
+          }},
+          widthConstraint: { maximum: 150 }
+        });
+      }
+      catch(error) {
+        console.log(error);
+          //errors if there are duplicate items
+      }
     }
     
   });
-  x = y.distinct('id');
-  var items = y.get(x);
-  nodes.add(items);
 
   json.forEach(element => {
     edges.add({from: element.OBJECT_NAME, to: element.GRANTED_TO_NAME, arrows:'to'})
