3cadd676a9213d76a49f3d809f74ff61ee9cdcce
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2020-04-23 17:33:39 -0500

    update style

--- src/main.js
+++ src/main.js
@@ -15,13 +15,107 @@ var data = {
 };
 var options = {
   layout:{
-    improvedLayout: true,
-    hierarchical: true,
-  }
+    //improvedLayout: true,
+    hierarchical: {
+      nodeSpacing: 180
+    }
+  },
+  physics: false,
+  interaction:{
+    hover:true
+  },
+  edges: {
+    color: {
+      color:'rgb(211, 211, 211, 0.1)',
+      highlight:'blue',
+      hover: 'rgb(35, 120, 249, 1)',
+      inherit: 'from',
+      opacity: 1
+    },
+  },
+  shadow:{
+    enabled: true,
+    color: 'rgba(0,0,0,0.5)',
+    size:10,
+    x:5,
+    y:5
+  },
 };
 
 // initialize network!
 var network = new vis.Network(container, data, options);
+
+let oldClickedNodeIds = [];
+let oldClickedEdgeIds = [];
+//click handler
+network.on( 'select', function(properties) {
+  if(oldClickedNodeIds.length > 0 ) {
+      let oldNodes = nodes.get(oldClickedNodeIds)
+      oldNodes.forEach(node => {
+        node.color = 
+        { 
+          border: 'rgb(211, 211, 211, 0.1)', 
+          background: node.initialColor,
+          highlight: 
+          {
+            border: '#2B7CE9',
+            background: 'yellow'
+          },
+          hover: {
+            border: '#2B7CE9',
+            background: 'lightblue'
+          }
+        }
+      })
+      nodes.update(oldNodes);
+  }
+  if(oldClickedEdgeIds.length > 0 ) {
+      let oldEdges = edges.get(oldClickedEdgeIds)
+      oldEdges.forEach(edge => {
+        edge.color = {
+          color:'rgb(211, 211, 211, 0.1)',
+          highlight:'blue',
+          hover: 'blue',
+          inherit: 'from',
+          opacity: 0.5
+        }
+      })
+      edges.update(oldEdges);
+  }
+  const clickedId = properties.nodes[0];
+   const connectedNodes = network.getConnectedNodes(clickedId)
+   const connectedEdges = network.getConnectedEdges(clickedId)
+
+//   connectedNodes.push(clickedId);
+//   nodes.update({id: clickedId, color: {background: "orange"}});
+//   network.selectNodes(connectedNodes);
+
+  // network.selectEdges(connectedEdges);
+   connectedNodes.forEach(nodeId => {
+   // network.clustering.updateEdge(originalEdge.id, {color : '#aa0000'})
+    nodes.update({id: nodeId, color: {background: "rgb(113, 167, 247, 1)"}});
+  })
+  connectedEdges.forEach(edgeId => {
+    edges.update({id: edgeId, color: "blue" });
+  })
+  oldClickedNodeIds = connectedNodes;
+  oldClickedEdgeIds = connectedEdges;
+ network.redraw();
+});
+
+// network.on( 'deselectNode', function(properties) {
+
+//   const previousIds = properties.previousSelection.nodes;
+//   const connectedNodes = network.getConnectedNodes(clickedId)
+
+//   connectedNodes.forEach(nodeId => {
+//     nodes.update({id: nodeId, color: {background: "orange"}});
+//   })
+// //  network.redraw();
+// });
+
+
+
 
 //load data
 fetch("http://localhost:1111/src/grants.json")
@@ -31,33 +125,39 @@ fetch("http://localhost:1111/src/grants.json")
   });
 
 var getColor = function(type) {
-  const types = [{objType: "ROLE", color: 'lightgreen'}, 
-                 {objType: "USER", color: '#D2E5FF'}];
-  const x = types.find(element => element.objType === type).color;
-  if(type === 'USER'){
-    console.log(type);
+  var color;
+  if(type === "ROLE"){
+    color = "rgb(255, 232, 232, 1)";
   }
-  return x;
+  else if(type === "USER"){
+    color = "rgb(189, 243, 227, 1)";
+  }
+  return color;
 }
 
 var render = function(json){
   json.forEach(element => {    
-    if((element.GRANTED_TO_TYPE === 'USER' || element.GRANTED_TO_TYPE === 'ROLE' ) && (element.SNOWFLAKE_OBJECT_TYPE === 'ROLE'  || element.SNOWFLAKE_OBJECT_TYPE === 'USER')){   
+    if((element.SNOWFLAKE_OBJECT_TYPE === 'ROLE') && (element.GRANTED_TO_NAME !=="SECURITYADMIN")) {   
       try {
         nodes.add({
           id: element.GRANTED_TO_NAME, 
           label: element.GRANTED_TO_NAME, 
           shape: "box", 
           color: { 
-            border: '#2B7CE9', 
+            border: 'rgb(211, 211, 211, 1)', 
             background: getColor(element.GRANTED_TO_TYPE),
             highlight: 
             {
+              border: 'rgb(211, 211, 211, 1)',
+              background: 'yellow'
+            },
+            hover: {
               border: '#2B7CE9',
-              background: '#D2E5FF'
+              background: 'lightblue'
             }
           },
-          widthConstraint: { maximum: 150 }
+          widthConstraint: { maximum: 150 },
+          initialColor: getColor(element.GRANTED_TO_TYPE)
         });
       }
       catch(error) {
@@ -70,14 +170,19 @@ var render = function(json){
           label: element.OBJECT_NAME, 
           shape: "box", 
           color: { 
-            border: '#2B7CE9', 
+            border: 'rgb(211, 211, 211, 0.1)', 
             background: getColor(element.SNOWFLAKE_OBJECT_TYPE),
             highlight: {
               border: '#2B7CE9',
-              background: '#D2E5FF'
+              background: 'yellow'
+            },
+            hover: {
+              border: '#2B7CE9',
+              background: 'lightblue'
             }
           },
-          widthConstraint: { maximum: 150 }
+          widthConstraint: { maximum: 150 },
+          initialColor: getColor(element.SNOWFLAKE_OBJECT_TYPE)
         });
       }
       catch(error) {
@@ -89,6 +194,8 @@ var render = function(json){
   });
 
   json.forEach(element => {
-    edges.add({from: element.OBJECT_NAME, to: element.GRANTED_TO_NAME, arrows:'to'})
+    if((element.GRANTED_TO_NAME !=="SECURITYADMIN") && (element.PRIVILEGE !== 'OWNERSHIP')) {
+      edges.add({from: element.OBJECT_NAME, to: element.GRANTED_TO_NAME, arrows:'to'})
+    }
   })
 }
