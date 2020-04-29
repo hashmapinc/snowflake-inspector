b4b539c57b8bf07a8f1b2fb49a4bc658f0a2332a
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2020-04-29 09:31:54 -0500

    Highlight nodes all the way to the last parent

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
 'use strict';
 
-var nodes = new vis.DataSet([]);
-
-// create an array with edges
-var edges = new vis.DataSet([]);
-
 // create a network
 var container = document.getElementById('mynetwork');
 
-// provide the data in the vis format
-var data = {
-  nodes: nodes,
-  edges: edges
-};
 var options = {
+  nodes: {
+    shadow:{
+      enabled: true,
+      color: 'rgba(0,0,0,0.5)',
+      size:10,
+      x:5,
+      y:5
+    }
+  },
   layout:{
     //improvedLayout: true,
     hierarchical: {
-      nodeSpacing: 180
+      enabled: true,
+      nodeSpacing: 180,
+      sortMethod: 'directed'
     }
   },
   physics: false,
@@ -31,77 +32,14 @@ var options = {
       hover: 'rgb(35, 120, 249, 1)',
       inherit: 'from',
       opacity: 1
-    },
+    }
   },
-  shadow:{
-    enabled: true,
-    color: 'rgba(0,0,0,0.5)',
-    size:10,
-    x:5,
-    y:5
-  },
+  width: '1800px'
 };
-
-// initialize network!
-var network = new vis.Network(container, data, options);
 
 let oldClickedNodeIds = [];
 let oldClickedEdgeIds = [];
-//click handler
-network.on( 'select', function(properties) {
-  if(oldClickedNodeIds.length > 0 ) {
-      let oldNodes = nodes.get(oldClickedNodeIds)
-      oldNodes.forEach(node => {
-        node.color = 
-        { 
-          border: 'rgb(211, 211, 211, 1)', 
-          background: node.initialColor,
-          highlight: 
-          {
-            border: '#2B7CE9',
-            background: 'yellow'
-          },
-          hover: {
-            border: '#2B7CE9',
-            background: 'lightblue'
-          }
-        }
-      })
-      nodes.update(oldNodes);
-  }
-  if(oldClickedEdgeIds.length > 0 ) {
-      let oldEdges = edges.get(oldClickedEdgeIds)
-      oldEdges.forEach(edge => {
-        edge.color = {
-          color:'rgb(211, 211, 211, 0.1)',
-          highlight:'blue',
-          hover: 'blue',
-          inherit: 'from',
-          opacity: 0.5
-        }
-      })
-      edges.update(oldEdges);
-  }
-  const clickedId = properties.nodes[0];
-   const connectedNodes = network.getConnectedNodes(clickedId)
-   const connectedEdges = network.getConnectedEdges(clickedId)
 
-//   connectedNodes.push(clickedId);
-//   nodes.update({id: clickedId, color: {background: "orange"}});
-//   network.selectNodes(connectedNodes);
-
-  // network.selectEdges(connectedEdges);
-   connectedNodes.forEach(nodeId => {
-   // network.clustering.updateEdge(originalEdge.id, {color : '#aa0000'})
-    nodes.update({id: nodeId, color: {background: "rgb(113, 167, 247, 1)"}});
-  })
-  connectedEdges.forEach(edgeId => {
-    edges.update({id: edgeId, color: "blue" });
-  })
-  oldClickedNodeIds = connectedNodes;
-  oldClickedEdgeIds = connectedEdges;
- network.redraw();
-});
 
 // network.on( 'deselectNode', function(properties) {
 
@@ -114,11 +52,8 @@ network.on( 'select', function(properties) {
 // //  network.redraw();
 // });
 
-
-
-
 //load data
-fetch("http://localhost:1111/src/grants.json")
+fetch("http://localhost:1111/src/granted1.json")
   .then(response => response.json())
   .then(json => {
     render(json);
@@ -134,6 +69,8 @@ var getColor = function(type) {
   }
   return color;
 }
+
+var nodes = new vis.DataSet([]);
 
 var render = function(json){
   json.forEach(element => {    
@@ -193,9 +130,97 @@ var render = function(json){
     
   });
 
+  let x = []
+// create an array with edges
+
   json.forEach(element => {
     if((element.GRANTED_TO_NAME !=="SECURITYADMIN") && (element.PRIVILEGE !== 'OWNERSHIP')) {
-      edges.add({from: element.OBJECT_NAME, to: element.GRANTED_TO_NAME, arrows:'to'})
+      x.push({from: element.OBJECT_NAME, to: element.GRANTED_TO_NAME, arrows:'to'});
+     // edges.add({from: element.OBJECT_NAME, to: element.GRANTED_TO_NAME, arrows:'to'})
     }
   })
+  
+  var edges = new vis.DataSet(x);
+
+  // provide the data in the vis format
+  var data = {
+    nodes: nodes,
+    edges: edges
+  };
+    // initialize network!
+ var network = new vis.Network(container, data, options);
+
+ let getLinkedNodes = function(nodeId, nodeArray, edgeArray){ 
+   if(nodeId) {
+      let childEdges = network.getConnectedEdges(nodeId);
+      let directedEdges = edges.get(childEdges)
+          .filter((edge) => { return edge.to === nodeId })
+
+      directedEdges.map(edge => {
+        edge.color = 'blue';
+      })
+      directedEdges.forEach( edge => {
+        edgeArray.push(edge)
+      })
+
+     edges.update(directedEdges);
+
+    let childNodes = network.getConnectedNodes(nodeId, 'from');
+    if(childNodes) {
+      childNodes.forEach(child => {
+        if(nodeArray.indexOf(child) === -1){
+          nodeArray.push(child);
+          nodes.update({id: child, color: {background: "rgb(113, 167, 247, 1)"}});
+          getLinkedNodes(child, nodeArray, edgeArray);       
+        }
+      })
+    }
+   }
+   return [nodeArray, edgeArray];
+ }
+
+ //click handler
+  network.on( 'select', function(properties) {
+  if(oldClickedNodeIds.length > 0 ) {
+      let oldNodes = nodes.get(oldClickedNodeIds)
+      oldNodes.forEach(node => {
+        node.color = 
+        { 
+          border: 'rgb(211, 211, 211, 1)', 
+          background: node.initialColor,
+          highlight: 
+          {
+            border: '#2B7CE9',
+            background: 'yellow'
+          },
+          hover: {
+            border: '#2B7CE9',
+            background: 'lightblue'
+          }
+        }
+      })
+      nodes.update(oldNodes);
+  }
+  if(oldClickedEdgeIds.length > 0 ) {
+     oldClickedEdgeIds.forEach(edge => {
+        edge.color = {
+          color:'rgb(211, 211, 211, 0.1)',
+          highlight:'blue',
+          hover: 'blue',
+          inherit: 'from',
+          opacity: 0.5
+        }
+      })
+      edges.update(oldClickedEdgeIds );
+  }
+  const clickedId = properties.nodes[0];
+  const connectedNodes = getLinkedNodes(clickedId, [], []);
+
+  oldClickedNodeIds = connectedNodes[0];
+  oldClickedEdgeIds = connectedNodes[1];
+ //network.redraw();
+  });
+  window.onresize = function() {network.fit();}
+
 }
+
