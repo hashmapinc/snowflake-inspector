d33de58ba398cdccffcc6f2c0052a80e0ae6cf9f
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2020-06-09 19:34:57 -0500

    Extract network viz component

--- src/js/network-vis.js
+++ src/js/network-vis.js
@@ -1,19 +1,19 @@
 import { Network } from 'vis-network/peer/esm/vis-network';
 import { DataSet } from 'vis-data/peer/esm/vis-data';
-import { colors } from './constants';
+import { COLORS } from './constants';
 import 'jstree/dist/themes/default/style.css';
 import jstree from 'jstree';
 import renderHierarchy from './hierarchy-vis';
 
 // get vis div
-const hierarchy_vis_div = document.getElementById('network-vis');
+const networkVisDiv = document.getElementById('network-vis');
 
 // set vis options
 const options = {
   nodes: {
     shadow: {
       enabled: true,
-      color: colors.nodeShadow,
+      color: COLORS.nodeShadow,
       size: 10,
       x: 5,
       y: 5,
@@ -32,9 +32,9 @@ const options = {
   },
   edges: {
     color: {
-      color: colors.edge,
-      highlight: colors.edgeHighlight,
-      hover: colors.hover,
+      color: COLORS.edge,
+      highlight: COLORS.edgeHighlight,
+      hover: COLORS.hover,
       inherit: 'from',
       opacity: 1,
     },
@@ -48,130 +48,40 @@ let oldClickedEdgeIds = [];
 const getColor = function (type) {
   let color;
   if (type === 'ROLE') {
-    color = colors.roleNode;
+    color = COLORS.roleNode;
   } else if (type === 'USER') {
-    color = colors.userNode;
+    color = COLORS.userNode;
   }
   return color;
 };
 
-export function render(json) {
-  let nodes = new DataSet([]),
-    hierarchy = [],
-    database = {},
-    schema = {},
-    category = {},
-    item = {},
-    other = {};
-  json.forEach((element) => {
-    if (
-      element.GRANTED_ON_TYPE === 'ROLE' &&
-      element.GRANTED_TO_NAME !== 'SECURITYADMIN' &&
-      element.PRIVILEGE !== 'OWNERSHIP'
-    ) {
-      try {
-        nodes.add({
-          id: element.GRANTED_TO_NAME,
-          label: element.GRANTED_TO_NAME,
-          shape: 'box',
-          color: {
-            border: colors.nodeBorder,
-            background: getColor(element.GRANTED_TO_TYPE),
-            highlight: {
-              border: colors.hoverBorder,
-              background: colors.highlight,
-            },
-            hover: {
-              border: colors.hoverBorder,
-              background: colors.hover,
-            },
-          },
-          widthConstraint: { maximum: 150 },
-          initialColor: getColor(element.GRANTED_TO_TYPE),
-        });
-      } catch (error) {
-        //errors if there are duplicate items
-        console.log('Encountered error: ' + error);
-      }
-      try {
-        nodes.add({
-          id: element.GRANTED_ON_NAME,
-          label: element.GRANTED_ON_NAME,
-          shape: 'box',
-          color: {
-            border: colors.nodeBorder,
-            background: getColor(element.GRANTED_ON_TYPE),
-            highlight: {
-              border: colors.peach,
-              background: colors.highlight,
-            },
-            hover: {
-              border: colors.hoverBorder,
-              background: colors.hover,
-            },
-          },
-          widthConstraint: { maximum: 150 },
-          initialColor: getColor(element.GRANTED_ON_TYPE),
-        });
-      } catch (error) {
-        //errors if there are duplicate items
-        console.log('Encountered error: ' + error);
-      }
-    } else if (element.GRANTED_ON_DATABASE) {
-      const index = hierarchy.findIndex((db) => db.text === element.GRANTED_ON_DATABASE);
-      database = index !== -1 ? hierarchy[index] : { text: element.GRANTED_ON_DATABASE, children: [] };
+const renderNetwork = (dataNodes, dataEdges) => {
+  let nodes = new DataSet([]);
 
-      if (element.GRANTED_ON_SCHEMA) {
-        let schemaIndex = database.children.findIndex((schema) => schema.text === element.GRANTED_ON_SCHEMA);
-        schema =
-          schemaIndex !== -1 ? database.children[schemaIndex] : { text: element.GRANTED_ON_SCHEMA, children: [] };
-
-        if (element.GRANTED_ON_TYPE && element.GRANTED_ON_TYPE !== 'SCHEMA') {
-          let categoryIndex = schema.children.findIndex((category) => category.text === element.GRANTED_ON_TYPE);
-          category =
-            categoryIndex !== -1 ? schema.children[categoryIndex] : { text: element.GRANTED_ON_TYPE, children: [] };
-          categoryIndex === -1 ? schema.children.push(category) : (schema.children[categoryIndex] = category);
-
-          let itemIndex = category.children.findIndex((item) => item.text === element.GRANTED_ON_NAME);
-          item = itemIndex !== -1 ? category.children[itemIndex] : { text: element.GRANTED_ON_NAME, children: [] };
-          itemIndex === -1 ? category.children.push(item) : (category.children[itemIndex] = item);
-        }
-
-        schemaIndex === -1 ? database.children.push(schema) : (database.children[schemaIndex] = schema);
-      }
-      index === -1 ? hierarchy.push(database) : (hierarchy[index] = database);
-    } else {
-      let otherIndex = hierarchy.findIndex(
-        (other) => other.text === '(' + element.GRANTED_ON_TYPE + ')' + element.GRANTED_ON_NAME
-      );
-      other =
-        otherIndex !== -1
-          ? hierarchy[otherIndex]
-          : {
-              text: '(' + element.GRANTED_ON_TYPE + ')' + element.GRANTED_ON_NAME,
-              type: element.GRANTED_ON_TYPE,
-              children: [],
-            };
-
-      otherIndex === -1 ? hierarchy.push(other) : (hierarchy[otherIndex] = other);
-    }
+  dataNodes.forEach((element) => {
+    nodes.add({
+      id: element.name,
+      label: element.name,
+      shape: 'box',
+      color: {
+        border: COLORS.nodeBorder,
+        background: getColor(element.type),
+        highlight: {
+          border: COLORS.hoverBorder,
+          background: COLORS.highlight,
+        },
+        hover: {
+          border: COLORS.hoverBorder,
+          background: COLORS.hover,
+        },
+      },
+      widthConstraint: { maximum: 150 },
+      initialColor: getColor(element.type),
+    });
   });
-  hierarchy.sort((a, b) => (a.text > b.text ? -1 : 1));
-  renderHierarchy(hierarchy);
   // create an array with edges
-  let x = [];
 
-  json.forEach((element) => {
-    if (element.GRANTED_TO_NAME !== 'SECURITYADMIN' && element.PRIVILEGE !== 'OWNERSHIP') {
-      x.push({
-        from: element.GRANTED_TO_NAME,
-        to: element.GRANTED_ON_NAME,
-        arrows: 'to',
-      });
-    }
-  });
-
-  let edges = new DataSet(x);
+  let edges = new DataSet(dataEdges);
 
   // provide the data in the vis format
   let data = {
@@ -180,7 +90,7 @@ export function render(json) {
   };
 
   // initialize network!
-  let network = new Network(hierarchy_vis_div, data, options);
+  let network = new Network(networkVisDiv, data, options);
 
   let getLinkedNodes = (nodeId, nodeArray = [], edgeArray = [], direction = null) => {
     const selectNodesRecursively = (connectedNodes, direction, background, border) => {
@@ -219,32 +129,32 @@ export function render(json) {
 
         let parentNodes = network.getConnectedNodes(nodeId, 'from');
         let childNodes = network.getConnectedNodes(nodeId, 'to');
-        selectNodesRecursively(parentNodes, 'parent', colors.parentSelectedBackground, colors.parentSelectedBorder);
-        selectNodesRecursively(childNodes, 'child', colors.childSelectedBackground, colors.childSelectedBorder);
+        selectNodesRecursively(parentNodes, 'parent', COLORS.parentSelectedBackground, COLORS.parentSelectedBorder);
+        selectNodesRecursively(childNodes, 'child', COLORS.childSelectedBackground, COLORS.childSelectedBorder);
       } else if (direction === 'parent') {
         parentEdges = edges.get(childEdges).filter((edge) => {
           return edge.to === nodeId;
         });
         let parentNodes = network.getConnectedNodes(nodeId, 'from');
-        selectNodesRecursively(parentNodes, 'parent', colors.parentSelectedBackground, colors.parentSelectedBorder);
+        selectNodesRecursively(parentNodes, 'parent', COLORS.parentSelectedBackground, COLORS.parentSelectedBorder);
       } else if (direction === 'child') {
         childEdges = edges.get(childEdges).filter((edge) => {
           return edge.from === nodeId;
         });
         let childNodes = network.getConnectedNodes(nodeId, 'to');
-        selectNodesRecursively(childNodes, 'child', colors.childSelectedBackground, colors.childSelectedBorder);
+        selectNodesRecursively(childNodes, 'child', COLORS.childSelectedBackground, COLORS.childSelectedBorder);
       }
 
       if (parentEdges && parentEdges.length > 0) {
         parentEdges.map((edge) => {
-          edge.color = colors.parentEdge;
+          edge.color = COLORS.parentEdge;
           edgeArray.push(edge);
         });
         edges.update(parentEdges);
       }
       if (childEdges && childEdges.length > 0) {
         childEdges.map((edge) => {
-          edge.color = colors.childEdge;
+          edge.color = COLORS.childEdge;
           edgeArray.push(edge);
         });
         edges.update(childEdges);
@@ -259,15 +169,15 @@ export function render(json) {
       let oldNodes = nodes.get(oldClickedNodeIds);
       oldNodes.forEach((node) => {
         node.color = {
-          border: colors.nodeBorder,
+          border: COLORS.nodeBorder,
           background: node.initialColor,
           highlight: {
-            border: colors.hoverBorder,
-            background: colors.highlight,
+            border: COLORS.hoverBorder,
+            background: COLORS.highlight,
           },
           hover: {
-            border: colors.hoverBorder,
-            background: colors.hover,
+            border: COLORS.hoverBorder,
+            background: COLORS.hover,
           },
         };
       });
@@ -276,9 +186,9 @@ export function render(json) {
     if (oldClickedEdgeIds.length > 0) {
       oldClickedEdgeIds.forEach((edge) => {
         edge.color = {
-          color: colors.edge,
-          highlight: colors.edgeHighlight,
-          hover: colors.hover,
+          color: COLORS.edge,
+          highlight: COLORS.edgeHighlight,
+          hover: COLORS.hover,
           inherit: 'from',
           opacity: 1,
         };
@@ -295,4 +205,6 @@ export function render(json) {
   window.onresize = function () {
     network.fit();
   };
-}
+};
+
+export default renderNetwork;
