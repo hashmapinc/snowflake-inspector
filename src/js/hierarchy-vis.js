13e9fb5365be315c74f4e661f1f9ff8a1493b5f7
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2020-06-12 13:34:16 -0500

    auto expand

--- src/js/hierarchy-vis.js
+++ src/js/hierarchy-vis.js
@@ -1,10 +1,14 @@
 const renderHierarchy = (tree) => {
   $('#hierarchy').data('jstree', false).empty();
-  $('#hierarchy').jstree({
-    core: {
-      data: tree,
-    },
-  });
+  $('#hierarchy')
+    .jstree({
+      core: {
+        data: tree,
+      },
+    })
+    .on('loaded.jstree', function () {
+      $('#hierarchy').jstree('open_all');
+    });
 };
 
 export default renderHierarchy;
