6cc1c66b1cb538ea91392a832930d4a25d7693c8
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2020-06-15 09:44:24 -0500

    Get json tree

--- src/js/hierarchy-vis.js
+++ src/js/hierarchy-vis.js
@@ -16,6 +16,10 @@ const renderHierarchy = (tree) => {
     $('#hierarchy').jstree(true).settings.core.data = tree;
     $('#hierarchy').jstree(true).refresh();
   }
+  $('#hierarchy').on('select_node.jstree', function (e, data) {
+    console.log('data', data.instance.get_node(data.selected[0]));
+    console.log('json', data.instance.get_json());
+  });
 };
 
 export default renderHierarchy;
