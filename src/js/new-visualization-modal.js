8b61166ba7d3d051ee97ae4a8f34a05cbca4fe64
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2020-08-19 13:57:51 -0500

    File upload feature for query results

--- src/js/new-visualization-modal.js
+++ src/js/new-visualization-modal.js
@@ -2,41 +2,34 @@ import * as app from './app';
 
 import dataQuery from '../data/data_query.sql';
 
+let queryResults = null;
 // handle modal submit
 function onNewVisSubmit() {
   // remove old warnings
-  $('#invalid-query-results-warning').hide();
-  $('#empty-query-results-warning').hide();
-
-  // get query results
-  let queryResults = $('textarea#data-input').val().trim();
-
-  // validate results
-  if (queryResults === '') {
+  hideAllWarnings();
+  if (!queryResults) {
     $('#empty-query-results-warning').show();
-    return;
   }
 
-  // parse the text into a hierarchy array
-  if (queryResults.startsWith('GRANTS')) {
-    const index = queryResults.indexOf('[');
-    queryResults = queryResults.substring(index);
-  } else if (!queryResults.startsWith('[')) {
-    $('#invalid-query-results-warning').show();
-    return;
-  }
-
-  // plot the visualization
-  $('#create-vis-modal').modal('toggle');
   try {
-    app.init(JSON.parse(queryResults));
+    app.init(queryResults);
+    $('#create-vis-modal').modal('toggle');
   } catch (error) {
+    $('#invalid-query-results-warning').show();
     alert(
       "Whoops! We couldn't parse your results. Please double check your query and try again. Please submit feedback if you believe this is a bug."
     );
   }
 }
+//hide warning on modal open
+$('#create-vis-modal').on('shown.bs.modal', function (e) {
+  hideAllWarnings();
+});
 
+function hideAllWarnings() {
+  $('#invalid-query-results-warning').hide();
+  $('#empty-query-results-warning').hide();
+}
 // handle copy button select
 function onCopyQueryClick() {
   // Select the query element and copy it's contents
@@ -54,7 +47,54 @@ export default function initModal() {
   // add SQL query to the modal
   document.getElementById('data-query').innerHTML = dataQuery;
 
+  hideAllWarnings();
+
   // wire up event listeners
   $('#new-vis-button').click(onNewVisSubmit);
   $('#copy-query-button').click(onCopyQueryClick);
 }
+
+const csvStringToArray = (strData) => {
+  const objPattern = new RegExp('(\\,|\\r?\\n|\\r|^)(?:"([^"]*(?:""[^"]*)*)"|([^\\,\\r\\n]*))', 'gi');
+  // Create an array to hold our individual pattern
+  // matching groups.
+  let arrMatches = null,
+    // Create an array to hold our data. Give the array
+    // a default empty first row.
+    arrData = [[]];
+  // Keep looping over the regular expression matches
+  // until we can no longer find a match.
+  while ((arrMatches = objPattern.exec(strData))) {
+    if (arrMatches[1].length && arrMatches[1] !== '\\') arrData.push([]);
+    arrData[arrData.length - 1].push(arrMatches[2] ? arrMatches[2].replace(new RegExp('""', 'g'), '"') : arrMatches[3]);
+  }
+  return arrData;
+};
+
+function readSingleFile(evt) {
+  let f = evt.target.files[0];
+  if (f) {
+    let r = new FileReader();
+    r.onload = function (e) {
+      let contents = e.target.result;
+      let lines = csvStringToArray(contents);
+      lines.shift();
+
+      try {
+        queryResults = lines.map((line) => {
+          return JSON.parse(line);
+        });
+        hideAllWarnings();
+      } catch (error) {
+        $('#invalid-query-results-warning').show();
+        alert(
+          "Whoops! We couldn't parse your results. Please double check your query and try again. Please submit feedback if you believe this is a bug."
+        );
+      }
+    };
+    r.readAsText(f);
+  } else {
+    alert('Failed to load file');
+  }
+}
+document.getElementById('fileinput').addEventListener('change', readSingleFile);
