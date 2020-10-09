b3519228e636581a029c2a2fc32f18981f392ebf
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2020-10-09 15:36:21 -0500

    Dynamicly loading sample data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-import sample_data from './data/sample-data.json';
 import * as app from './js/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
@@ -8,10 +7,31 @@ import 'bootstrap';
 import jstree from 'jstree';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import initModal from './js/new-visualization-modal.js';
+import { csvStringToArray } from './js/new-visualization-modal';
 
 // main
 document.addEventListener('DOMContentLoaded', function () {
   initModal();
-  //load sample data
-  app.init(sample_data);
+  loadSampleData();
 });
+
+const loadSampleData = () => {
+  const URL = window.location.host + '/sample-data.csv';
+
+  /*  
+    If you have the external URL for the file, you could hardcode the URL like this -  fetch('http://snowflakeinspector.hashmapinc.com/sample-data.csv')
+  */
+  fetch('http://' + URL)
+    .then((response) => response.text())
+    .then((data) => {
+      let lines = csvStringToArray(data);
+      //remove 1st element which is the header of CSV file
+      lines.shift();
+
+      let queryResults = lines.map((line) => {
+        return JSON.parse(line);
+      });
+
+      app.init(queryResults);
+    });
+};
--- src/js/new-visualization-modal.js
+++ src/js/new-visualization-modal.js
@@ -111,3 +111,5 @@ function readSingleFile(event) {
 }
 
 $('#fileinput').on('change', readSingleFile);
+
+export { csvStringToArray };
